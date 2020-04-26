019c6cc8a5bcd27c9f8a0ab784a7ab1caa4c6d75
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-04-26 15:26:01 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = function(){
+    let bst = require("./DataStructers/BinarySearchTrees");
+    let dll = require("./DataStructers/DoublyLinkedList");
+    let ll = require("./DataStructers/LinkedList");
+    let q = require("./DataStructers/Queue");
+    let ql = require("./DataStructers/QueueLinked");
+    let pq = require("./DataStructers/PriorityQueue");
+    let s = require("./DataStructers/Stack");
+    let hs = require("./DataStructers/HashTable");
+
+    this.DoublyLinkedList = function(){
+        return new dll.doublyLinkedList();
+    }
+
+    this.LinkedList = function(){
+        return new ll.linkedList();
+    }
+
+    this.Queue = function(){
+        return new q.Queue(); 
+    }
+
+    this.QueueLinked = function(){
+        return new ql.LinkedQueue();
+    }
+
+    this.PriortyQueue = function(){
+        return new pq.PriorityQueue();
+    }
+
+    this.Stack = function(){
+        return new s.Stack();
+    }
+
+    this.BinarySerachTree = function(){
+        return new bst.BinarySearchTree();
+    }
+
+    this.HashTable = function(){
+        return new hs.HashTable();
+    }
+}
